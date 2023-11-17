8f4c45dec192970a525f7b3fa64559a787fc1d57
nu pan <135819902+unknown-nk@users.noreply.github.com>
2023-11-17 15:03:39 +0800

    feat: about RWD

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,14 +1,17 @@
 <template>
   <!-- 關於我 -->
-  <div class="h-[996px] flex justify-center">
-    <div class="w-pad desktop:w-mid max:w-desktop grid grid-cols-2 content-center justify-center">
-      <div class="grow">
+
+  <div class="h-[996px] flex justify-center mt-[50px] pad:mt-0">
+    <div
+      class="w-pad desktop:w-mid max:w-desktop pad:grid grid-cols-2 content-center justify-center relative pad:static"
+    >
+      <div class="grow text-center pad:text-start absolute pad:static">
         <span class="font-jost text-primary text-[30px]">About me</span>
-        <p class="text-[80px] text-sub-primary font-extrabold tracking-[.28em]">
+        <p class="text-[30px] mobile:text-[50px] pad:text-[80px] text-sub-primary font-extrabold tracking-[.28em]">
           立法委員<br />
           候選人主張
         </p>
-        <div class="mt-20 tracking-[.24em] leading-[30px]">
+        <div class="mx-8 mt-[450px] pad:mx-0 pad:mt-20 tracking-[.24em] leading-[30px]">
           <p>
             我堅信，藉由推動更完善的貓咪福利和相關政策，更是間接地投資於台灣的未來。畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。透過完善的貓咪福利政策，為台灣的
             GDP 經濟帶來巨大效益。
@@ -17,15 +20,24 @@
           </p>
         </div>
       </div>
-      <div class="grow relative justify-self-end">
-        <span class="absolute z-30 bottom-10 right-4">
-          <p class="text-[80px] text-sub-primary font-extrabold tracking-[.28em]">喵立翰</p>
-          <p class="text-[50px] text-sub-primary font-medium tracking-[.08em] font-jost">Miao Li-Han</p>
+      <div
+        class="grow w-[320px] pad:w-full pad:justify-self-end absolute top-48 mobile:top-60 left-[50%] -translate-x-1/2 pad:relative pad:top-0 pad:left-0 pad:translate-x-0"
+      >
+        <span class="absolute z-30 bottom-2 pad:bottom-10 right-0 desktop:right-4 text-end pad:text-center">
+          <p class="text-[24px] pad:text-[80px] text-sub-primary font-extrabold tracking-[.28em]">喵立翰</p>
+          <p class="text-[18px] pad:text-[50px] text-sub-primary font-medium tracking-[.08em] font-jost">Miao Li-Han</p>
         </span>
-        <img src="/images/img/about-fish.svg" alt="" class="absolute z-20 -bottom-24 right-0" />
-        <img src="/images/img/miao.png" alt="" class="" />
-        <!-- <div class="h-[630px] w-[630px]"><img src="/images/img/miao.png" alt="" /></div> -->
-        <img src="/images/img/Vector.png" alt="" class="absolute -z-50 -bottom-20 left-16" />
+        <img
+          src="/images/img/about-fish.svg"
+          alt=""
+          class="h-[58px] pad:h-auto absolute z-20 bottom-0 left-0 pad:-bottom-24 pad:left-auto pad:right-0"
+        />
+        <img src="/images/img/miao.png" alt="" class="h-[320px] pad:h-[520px] desktop:h-full object-cover" />
+        <img
+          src="/images/img/Vector.png"
+          alt=""
+          class="h-[235px] pad:h-[440px] desktop:h-full absolute -z-50 -bottom-4 left-8 desktop:-bottom-20 desktop:left-16"
+        />
       </div>
     </div>
   </div>
