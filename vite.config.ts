61ebf2ec441e31bc2783990e085d396539d5a0a8
nu pan <135819902+unknown-nk@users.noreply.github.com>
2023-11-19 00:16:10 +0800

    chore: 調整

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const srcPath = resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
+    base: '/candidate_meow/',
     plugins: [vue()],
     resolve: {
         alias: {
